e4f1608bdb960b01b9d10201d50bad31f776fc75
PanJx <panjx6@outlook.com>
2020-10-26 16:48:43 +0800

    finished regression.py

--- regression.py
+++ regression.py
@@ -1,7 +1,6 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 import os
+import numpy as np
+import pandas as pd
 
 
 def load_data(data_path):
@@ -12,10 +11,23 @@ def load_data(data_path):
 
 
 def solve(t, I, r_beta):
-    N, I0 = 0, 0
-    plt.plot(t, I)
-    plt.show()
-
+    x = np.exp(-r_beta * t)
+    y = 1 / I
+    phi0_phi0 = t.shape[0]
+    phi0_phi1 = sum(x)
+    phi1_phi1 = sum(x ** 2)
+    phi0_f = sum(y)
+    phi1_f = sum(x * y)
+    # 得到线性方程组
+    # phi0_phi0 * a0 + phi0_phi1 * a1 = phi0_f
+    # phi0_phi1 * a0 + phi1_phi1 * a1 = phi1_f
+    a0 = (phi0_f / phi0_phi1 * phi1_phi1 - phi1_f) / (phi0_phi0 / phi0_phi1 * phi1_phi1 - phi0_phi1)
+    a1 = (phi0_f / phi0_phi0 * phi0_phi1 - phi1_f) / (phi0_phi1 / phi0_phi0 * phi0_phi1 - phi1_phi1)
+    # 得到N和I0
+    # y = (1/I0-1/N) * x + 1/N
+    # 四舍五入取整
+    N = np.rint(1 / a0)
+    I0 = np.rint(1 / (a1 + a0))
     return N, I0
 
 
